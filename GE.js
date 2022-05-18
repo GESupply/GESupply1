af3967ffe5ce18bc8aa4892d95d084a1e5e43625
GESupply <nikoarsenashvili123@gmail.com>
2022-05-18 15:15:51 +0400

    updutu

--- GE.js
+++ GE.js
@@ -1,5 +1,49 @@
 
 
-console.log ("Welcome")
+var objs = document.getElementById ('objs')
+
+for (var obj of objects) {
+
+	var olink = document.createElement ('a')
+	olink.href = obj.link
+
+	var oimg = document.createElement ('img')
+	oimg.src = obj.img
+	oimg.className = 'imgs'
+
+	var oname = document.createElement ('p')
+	oname.innerHTML = obj.name
+
+	var ob = document.createElement ('div')
+	ob.className = 'ob'
+
+	ob.appendChild (oimg)
+	ob.appendChild (oname)
+	olink.appendChild (ob)
+
+	objs.appendChild (olink)
+
+	console.log (ob)
+}
+
+
+
+
+
+var ob = document.getElementsByClassName ('ob')
+
+style ()
+window.addEventListener ('resize', () => {
+	style ()
+})
+	
+function style () {
+
+	for (o of ob) {
+		o.style.height = (Math.floor (window.innerHeight * 0.3)).toString () + 'px'
+		o.style.marginLeft = '10px'
+
+	}
+}
 
 
---
+++ dbs.js
@@ -0,0 +1,16 @@
+
+
+var objects = [
+	{	
+		name: "Logitech Wireless Combo MK270",
+		img: "https://m.media-amazon.com/images/I/61pUul1oDlL._AC_SL1500_.jpg",
+		link: "https://www.amazon.com/Logitech-MK270-Wireless-Keyboard-Mouse/dp/B079JLY5M5/ref=lp_16225009011_1_10"
+	},
+	{
+		name: "Sceptre 24\" Professional Thin 75Hz 1080p LED Monitor 2x HDMI VGA Build-in Speakers, Machine Black",
+		img: "https://m.media-amazon.com/images/I/71rXSVqET9L._AC_SL1257_.jpg",
+		link: "https://www.amazon.com/Sceptre-E248W-19203R-Monitor-Speakers-Metallic/dp/B0773ZY26F/ref=lp_16225009011_1_12"
+	}
+]
+
+
